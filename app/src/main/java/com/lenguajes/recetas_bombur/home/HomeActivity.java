2b04600f78ca47d1fa5cdf8d72907b5f87d8ef6a
Mauricio Quriós <mqbravo@gmail.com>
2019-05-12 10:52:41 -0600

    Se inicia la GUI. Se añade la estructura de la vengtana principal y el card para las recetas

--- app/src/main/java/com/lenguajes/recetas_bombur/home/HomeActivity.java
+++ app/src/main/java/com/lenguajes/recetas_bombur/home/HomeActivity.java
@@ -1,13 +1,15 @@
-package com.lenguajes.recetas_bombur;
+package com.lenguajes.recetas_bombur.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.lenguajes.recetas_bombur.R;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
