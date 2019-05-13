57ecde28b7cd1879175f95a7a7caa66b9440c1a7
unknown <mqbravo@gmail.com>
2019-05-12 21:52:06 -0600

    Se extraen todas las dimensiones

---
+++ app/src/main/java/com/lenguajes/recetas_bombur/recipes/view/CreateRecipeActivity.java
@@ -0,0 +1,15 @@
+package com.lenguajes.recetas_bombur.recipes.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lenguajes.recetas_bombur.R;
+
+public class CreateRecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_recipe);
+    }
+}
