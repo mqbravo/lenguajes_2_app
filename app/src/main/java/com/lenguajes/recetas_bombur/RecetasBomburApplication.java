0bd2c9f270aef88f2f02dd727d3304fe4c827fb9
Feng <joseph.feng1@gmail.com>
2019-06-02 12:35:43 -0600

    Added global variable for the session token

--- app/src/main/java/com/lenguajes/recetas_bombur/RecetasBomburApplication.java
+++ app/src/main/java/com/lenguajes/recetas_bombur/RecetasBomburApplication.java
@@ -6,7 +6,7 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 public class RecetasBomburApplication extends Application {
-
+    public static String currentToken="";
     @Override
     public void onCreate() {
         super.onCreate();
@@ -14,5 +14,13 @@ public class RecetasBomburApplication extends Application {
 
     public static StorageReference getStorageReference(){
         return FirebaseStorage.getInstance().getReference();
+    }
+
+    public static void setCurrentToken(String newToken){
+        currentToken = newToken;
+    }
+
+    public static String getCurrentToken(){
+        return currentToken;
     }
 }
